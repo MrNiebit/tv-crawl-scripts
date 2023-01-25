c63b1314ab69ce705f874c61adc1e2030dc51451
gitsilence <gitsilence@lacknb.cn>
2023-01-25 19:51:29 +0800

    update model

--- py/plugin/model/py_model.py
+++ py/plugin/model/py_model.py
@@ -8,8 +8,12 @@ import json
 
 
 class Spider(Spider):
+
     def getName(self):
         return "Model"
+
+    def __init__(self):
+        pass
 
     def init(self, extend=""):
         pass
@@ -22,27 +26,111 @@ class Spider(Spider):
 
     def homeContent(self, filter):
         result = {}
+        cateManual = {
+            "电视剧": "dianshiju",
+        }
+        classes = []
+        for k in cateManual:
+            classes.append({
+                'type_name': k,
+                'type_id': cateManual[k]
+            })
+        result['class'] = classes
+        # if filter:
+        #     result['filters'] = self.config['filter']
         return result
 
     def homeVideoContent(self):
-        result = {}
-        return result
+        videos = []
+        sid = ""
+        name = ""
+        pic = ""
+        mark = ""
+        videos.append({
+            # sid /xxx.html
+            "vod_id": sid,
+            # 影片名
+            "vod_name": name,
+            # 图片地址
+            "vod_pic": pic,
+            # 更新集数
+            "vod_remarks": mark
+        })
+        return {'list': videos}
 
     def categoryContent(self, tid, pg, filter, extend):
-        result = {}
-        return result
+        videos = []
+        videos.append({
+            "vod_id": sid,
+            "vod_name": name,
+            "vod_pic": pic,
+            "vod_remarks": mark
+        })
+        return {
+            'list': videos,
+            'page': pg,
+            'pagecount': 99999,
+            'limit': 99,
+            'total': 999999
+        }
 
     def detailContent(self, array):
         result = {}
+        vod_play_url = '$$$'
+        playList = []
+        vodList = root.xpath("//div[@id='main0']")
+        for vl in vodList:
+            vodItems = []
+            aList = vl.xpath('./div/ul/li/a')
+            for tA in aList:
+                href = tA.xpath('./@href')[0]
+                name = tA.xpath('./text()')[0]
+                tId = self.regStr(href, '/v/(\\S+).html')
+                vodItems.append(name + "$" + tId)
+            joinStr = '#'
+            joinStr = joinStr.join(vodItems)
+            playList.append(joinStr)
+        vod = {
+            "vod_id": array[0],
+            "vod_name": title,
+            "vod_pic": pic,
+            "type_name": " ".join(root.xpath("//div[@class='sinfo']/span[3]//text()")),
+            "vod_year": " ".join(root.xpath("//div[@class='sinfo']/span[1]//text()")),
+            "vod_area": " ".join(root.xpath("//div[@class='sinfo']/span[2]//text()")),
+            "vod_remarks": root.xpath("//div[@class='sinfo']/p/text()")[0],
+            "vod_actor": "",
+            "vod_director": "",
+            "vod_content": root.xpath("//div[@class='area']/div[@class='fire l']/div[@class='info']/text()")[0]
+        }
+        vod['vod_play_from'] = vod_play_from
+        vod['vod_play_url'] = vod_play_url
+
+        result = {
+            'list': [
+                vod
+            ]
+        }
         return result
 
     def searchContent(self, key, quick):
-        result = {}
-        return result
+        videos = []
+        videos.append({
+            "vod_id": sid,
+            "vod_name": name,
+            "vod_pic": pic,
+            "vod_remarks": mark
+        })
+        return {
+            'list': videos
+        }
 
     def playerContent(self, flag, id, vipFlags):
-        result = {}
-        return result
+
+        return {
+            # parse 0: 直链  1：嗅探URL
+            'pare': 0,
+            'url': ''
+        }
 
     config = {
         "player": {},
